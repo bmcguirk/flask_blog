9d4a72dc43875c35150e98e2a4a9d0ec2e3efc00
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-08-19 16:16:17 +0000

    Enable migrations, admin login.

--- author/views.py
+++ author/views.py
@@ -1,8 +1,9 @@
 from flask_blog import app
-from flask import render_template, redirect, url_for, session, request
+from flask import render_template, redirect, url_for, session, request, flash
 from author.form import RegisterForm, LoginForm
 from author.models import Author
 from author.decorators import login_required
+import bcrypt
 
 @app.route('/login', methods=("GET","POST"))
 def login():
@@ -15,22 +16,25 @@ def login():
         pass
     if form.validate_on_submit():
         author = Author.query.filter_by(
-            username = form.username.data,
-            password = form.password.data
-            ).limit(1)
-        if author.count():
-            session['username'] = form.username.data
-            if 'next' in session:
-                next = session.get('next')
-                session.pop('next')
-                return redirect(next)
+            username = form.username.data
+            ).first()
+        if author:
+            if bcrypt.hashpw(form.password.data, author.password) == author.password:
+                session['username'] = form.username.data
+                session['is_author'] = author.is_author
+                if 'next' in session:
+                    next = session.get('next')
+                    session.pop('next')
+                    return redirect(next)
+                else:
+                    return redirect(url_for('login_success'))
             else:
-                return redirect(url_for('login_success'))
+                error = "Can't find that username/password combo."
         else:
             error = "Can't find that username/password combo."
-            return render_template('author/login.html', form=form, error=error)
     else:
-        return render_template('author/login.html', form=form, error=error)
+        pass
+    return render_template('author/login.html', form=form, error=error)
     
 @app.route('/register',methods=('GET','POST'))
 def register():
@@ -48,4 +52,10 @@ def success():
 @app.route('/login_success')
 @login_required
 def login_success():
-    return "Author logged in."
+    flash("Successful Login.")
+    return render_template('author/login_success.html')
+    
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('index'))
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from flask_blog import app, db
-from flask import render_template, redirect, flash, url_for
+from flask import render_template, redirect, flash, url_for, session, abort
 from blog.form import SetupForm
 from author.models import Author
 from blog.models import Blog
 from author.decorators import login_required
+import bcrypt
 
 @app.route('/')
 @app.route('/index')
@@ -18,18 +19,23 @@ def index():
 @app.route('/admin')
 @login_required
 def admin():
-    return render_template('blog/admin.html')
+    if session.get('is_author'):
+        return render_template('blog/admin.html')
+    else:
+        return abort(403)
 
 @app.route('/setup', methods=("GET","POST"))
 def setup():
     form = SetupForm()
     error = ""
     if form.validate_on_submit():
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(form.password.data, salt)
         author = Author(
             form.fullname.data,
             form.email.data,
             form.username.data,
-            form.password.data,
+            hashed_password,
             True)
         db.session.add(author)
         db.session.flush()
@@ -45,9 +51,9 @@ def setup():
             error = "Error creating user."
         if author.id and blog.id:
             db.session.commit()
-            flash("Blog and author created.")
-            return redirect(url_for('admin'))
         else:
             db.session.rollback()
             error = "Error creating blog."
+        flash("Blog and author created.")
+        return redirect(url_for('admin'))
     return render_template('blog/setup.html', form=form, error=error)
